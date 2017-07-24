d0040ad34e68db84ce9898569177597c20637362
hcruz <hugo.cruz@openbet.com>
2017-07-24 18:52:09 +0100

    End of day commit

--- src/recursion.clj
+++ src/recursion.clj
@@ -1,58 +1,127 @@
 (ns recursion)
 
 (defn product [coll]
-  :-)
+  (if (empty? coll)
+    1
+    (* (first coll)
+       (product (rest coll)))))
 
 (defn singleton? [coll]
-  :-)
+  (and (not (empty? coll))
+       (empty? (rest coll))))
 
 (defn my-last [coll]
-  :-)
+  (cond
+    (empty? coll) nil
+    (singleton? coll) (first coll)
+    :else (my-last (rest coll))))
 
 (defn max-element [a-seq]
-  :-)
+  (cond
+    (empty? a-seq) nil
+    (singleton? a-seq) (first a-seq)
+    :else (max (first a-seq)
+       (max-element (rest a-seq)))))
 
 (defn seq-max [seq-1 seq-2]
-  [:-])
+  (if (>(count seq-1) (count seq-2))
+    seq-1
+    seq-2
+    ))
 
 (defn longest-sequence [a-seq]
-  [:-])
+  (cond
+    (empty? a-seq) nil
+    (singleton? a-seq) (first a-seq)
+    :else (seq-max (first a-seq)
+       (longest-sequence (rest a-seq)))))
 
 (defn my-filter [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons
+            (first a-seq)
+            (my-filter pred? (rest a-seq))
+            )
+      (my-filter pred? (rest a-seq))
+    )))
 
 (defn sequence-contains? [elem a-seq]
-  :-)
+  (if (empty? a-seq)
+    false
+    (if (= elem (first a-seq))
+      true
+      (sequence-contains? elem (rest a-seq))
+    )))
 
 (defn my-take-while [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (cons
+            (first a-seq)
+            (my-take-while pred? (rest a-seq))
+            )
+      '()
+    )))
 
 (defn my-drop-while [pred? a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (if (pred? (first a-seq))
+      (my-drop-while pred? (rest a-seq))
+      a-seq
+    )))
 
 (defn seq= [a-seq b-seq]
-  :-)
+  (if (or (empty? a-seq) (empty? b-seq))
+    (= (count a-seq) (count b-seq))
+    (if (= (first a-seq) (first b-seq))
+      (seq= (rest a-seq) (rest b-seq))
+      false
+    )))
 
 (defn my-map [f seq-1 seq-2]
-  [:-])
+  (if (or (empty? seq-1) (empty? seq-2))
+    '()
+    (cons
+      (f (first seq-1) (first seq-2))
+      (my-map f (rest seq-1) (rest seq-2))
+    )))
 
 (defn power [n k]
-  :-)
+  (if (= k 0)
+    1
+    (* n (power n (dec k)))))
 
 (defn fib [n]
-  :-)
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else   (+ (fib (dec n)) (fib (- n 2)))
+    ))
 
 (defn my-repeat [how-many-times what-to-repeat]
-  [:-])
+  (if (<= how-many-times 0)
+    '()
+    (cons what-to-repeat (my-repeat (dec how-many-times) what-to-repeat))))
 
 (defn my-range [up-to]
-  [:-])
+  (if (<= up-to 0)
+    '()
+    (cons (dec up-to) (my-range (dec up-to)) )))
 
 (defn tails [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    (vector a-seq)
+    (cons (vec a-seq) (tails (rest a-seq)))))
 
 (defn inits [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    (vector a-seq)
+
+    (cons (vec a-seq) (inits (reverse (rest (reverse a-seq)))))))
 
 (defn rotations [a-seq]
   [:-])
